94b99a3131ac035bf981e5cc25169328247b716a
selfmadedigital <3jNfgDSf8@>
2019-08-02 08:26:17 +0200

    jwt test

--- src/app/_services/menu.service.ts
+++ src/app/_services/menu.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LinkModel } from '../_models/link';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {AuthenticationService} from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthenticationService) { }
 
   readMenu(): Observable<Object[]> {
-    return this.httpClient.get<Object[]>(environment.apiUrl + '/menu/');
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.authService.getAuthorizationToken()
+    });
+
+    return this.httpClient.get<Object[]>(environment.apiUrl + '/menu/', {headers: reqHeader});
   }
 
   updateMenu(menu: Object): Observable<boolean> {
